e199f2f5adf762bc3946b932f25ec4aa0fdce9ca
Aishwaryapt <86067623+Aishwaryapt@users.noreply.github.com>
2023-07-15 18:15:43 +0530

    Add files via upload

---
+++ featuresofjava/src/featuresofjava/example.java
@@ -0,0 +1,15 @@
+package featuresofjava;
+//Importing File Class
+import java.io.File;
+
+public class example {
+	public static void main(String[] args)
+	{
+
+		// File name specified
+		File obj = new File("myfile.txt");
+		System.out.println("File Created!");
+	}
+	
+
+}
